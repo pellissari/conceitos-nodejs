f67ee7b0bdbd0215a474c7cf248fd72ad4eb8c7f
Pellissari <andredoamaral@yahoo.com.br>
2020-12-11 11:54:18 -0300

    routes implementation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,62 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { url, title, techs } = request.body;
+
+  const repositorie = {id: uuid() , title, url, techs, likes: 0};
+
+  repositories.push(repositorie);
+
+  response.json(repositorie);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  const index = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (index < 0) {
+    return response.sendStatus(400);
+  } 
+
+  repositories[index] = { ...repositories[index], url, title, techs};
+
+  response.json(repositories[index]);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (index < 0) {
+    return response.sendStatus(400);
+  } 
+
+  repositories.splice(index,1);
+
+  response.sendStatus(204);
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorie = repositories.find(repositorie => repositorie.id === id);
+
+  if (!repositorie) {
+    return response.sendStatus(400);
+  } 
+  
+  repositorie.likes++;
+  response.json(repositorie);
 });
 
 module.exports = app;
